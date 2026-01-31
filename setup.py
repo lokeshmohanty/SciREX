0a3aa945effb7686bfc7be68d3f7eb731b7b92ca
Lokesh Mohanty <lokesh1197@gmail.com>
2026-01-31 23:40:55 +0530

    refactor: move `src/scirex` to `scirex`

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="scirex",
-    version="1.0.0",
-    packages=find_packages(),  # Remove the 'where' parameter
-    python_requires=">=3.10",  # Match your pyproject.toml requirement
-)
